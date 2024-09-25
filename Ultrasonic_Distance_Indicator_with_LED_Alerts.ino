8b796f7ae57eac12768b4bb0306a869d217164a1
Anayet Hossain <125878215+anayet884@users.noreply.github.com>
2024-09-25 09:22:31 +0600

    Add files via upload

---
+++ Ultrasonic_Distance_Indicator_with_LED_Alerts.ino
@@ -0,0 +1,43 @@
+const int trigPin = 3;
+const int echoPin = 6;
+long duration;
+int distance;
+int led1=7;
+int led2=9;
+int led3=10;
+
+void setup() {
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+pinMode(led1,OUTPUT);
+pinMode(led2,OUTPUT);
+pinMode(led3,OUTPUT);
+Serial.begin(9600);
+}
+
+
+void loop() 
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = duration * 0.034 / 2;
+
+if(distance<=10 && distance>1)
+digitalWrite(led1,HIGH);
+else
+digitalWrite(led1,LOW);
+if(distance<=20 && distance>10)
+digitalWrite(led2,HIGH);
+else
+digitalWrite(led2,LOW);
+if(distance>=30 && distance<50)
+digitalWrite(led3,HIGH);
+else
+digitalWrite(led3,LOW);
+Serial.print("Distance: ");
+Serial.println(distance);
+} 
